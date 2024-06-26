36adc6c942c27e81e3b869fdb57d51ebc75bc65f
Long Vu <longvu727@yahoo.com>
2024-06-26 13:13:46 -0700

    GameMicroservices: add getgamebygameid

--- app/get_game.go
+++ app/get_game.go
@@ -11,12 +11,17 @@ import (
 type GetGameParams struct {
 	GameID int32 `json:"game_id"`
 }
+
+type GetGameByGUIDParams struct {
+	GameGUID string `json:"game_guid"`
+}
+
 type GetGameResponse struct {
-	GameID   int32
-	GameGuid string
-	Sport    string
-	TeamA    string
-	TeamB    string
+	GameID   int32  `json:"game_id"`
+	GameGuid string `json:"game_guid"`
+	Sport    string `json:"sport"`
+	TeamA    string `json:"team_a"`
+	TeamB    string `json:"team_b"`
 
 	ErrorMessage string `json:"error_message"`
 }
@@ -32,6 +37,25 @@ func GetDBGame(ctx context.Context, request *http.Request, dbConnect *db.MySQL) 
 	json.NewDecoder(request.Body).Decode(&getGameParams)
 
 	gameRow, err := dbConnect.QUERIES.GetGame(ctx, getGameParams.GameID)
+	if err != nil {
+		return &getGameResponse, err
+	}
+
+	getGameResponse.GameID = gameRow.GameID
+	getGameResponse.GameGuid = gameRow.GameGuid
+	getGameResponse.Sport = gameRow.Sport.String
+	getGameResponse.TeamA = gameRow.TeamA.String
+	getGameResponse.TeamB = gameRow.TeamB.String
+
+	return &getGameResponse, nil
+}
+
+func GetGameByGUID(ctx context.Context, request *http.Request, dbConnect *db.MySQL) (*GetGameResponse, error) {
+	var getGameResponse GetGameResponse
+	var getGameByGUIDParams GetGameByGUIDParams
+	json.NewDecoder(request.Body).Decode(&getGameByGUIDParams)
+
+	gameRow, err := dbConnect.QUERIES.GetGameByGUID(ctx, getGameByGUIDParams.GameGUID)
 	if err != nil {
 		return &getGameResponse, err
 	}
